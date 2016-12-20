3ec0e7c57a98f26c06c8da868b146a59b39a83e4
Erik Wolters <erik-wolters@outlook.com>
2016-12-20 11:08:37 +0100

    Database
    Database code toegevoegd

---
+++ database/Database.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import java.sql.*;
+import sun.net.smtp.SmtpClient;
+
+/**
+ * Create a connection to the database.
+ *
+ * @author Erik Wolters <erik.wolters@hva.nl>
+ */
+public class Database {
+
+    private static final String DRIVER = "com.mysql.jdbc.Driver"; // DB Driver
+    private static final String URL_PREFIX = "jdbc:mysql://"; // DB URL Prefix
+
+    private String username = null; // The username for the connection
+    private String password = null; // The password for the connection
+    private String hostname = null; // The hostname of the server
+    private String database = null; // The database that conatains the tables
+
+    private Connection conn = null; // Database connection
+
+    /**
+     * Construcot to create a database with the default values
+     */
+    public Database() {
+        username = "fys";
+        password = "ESCXZoaIlK07pwUS";
+        hostname = "it95.nl:3306";
+        database = "fys";
+        createConnection();
+    }
+
+    /**
+     * Open a connection to the database
+     */
+    private void createConnection() {
+
+        try {
+            Class.forName(DRIVER);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            conn = DriverManager.getConnection(URL_PREFIX + hostname
+                    + "/" + database, username, password);
+            //return dbConnection;
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return;
+        }
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
+
+    public void close() {
+        conn = null;
+    }
+
+    /**
+     * Return all attributes of the database
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "Database{" + "username=" + username + ", password="
+                + password + ", hostname=" + hostname + ", database="
+                + database + '}';
+    }
+
+}
