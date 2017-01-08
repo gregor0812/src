bb95d4422e6561de007bb67a7153a584a9f4938b
gregor0812 <mikey0812@gmail.com>
2017-01-08 21:58:11 +0100

    Logging

---
+++ database/ConnectionFactory.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+/*
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.DriverManagerConnectionFactory;
+import org.apache.commons.dbcp.PoolableConnection;
+import org.apache.commons.dbcp.PoolableConnectionFactory;
+import org.apache.commons.dbcp.PoolingDataSource;
+import org.apache.commons.pool.impl.GenericObjectPool;
+*/
+
+/**
+ *
+ * @author Gregor
+ */
+public class ConnectionFactory {
+    
+}
