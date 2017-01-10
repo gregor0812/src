a506afdd1bb0e557c7d4c5416191b566f16d06c0
Michael <mikey0812@gmail.com>
2017-01-10 11:54:02 +0100

    connectionfactory fix

--- database/ConnectionFactory.java
+++ database/ConnectionFactory.java
@@ -1,8 +1,4 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
+//
 //package database;
 //
 //import java.sql.Connection;
@@ -17,10 +13,6 @@
 //import org.apache.commons.dbcp.PoolingDataSource;
 //import org.apache.commons.pool.impl.GenericObjectPool;
 //
-///**
-// *
-// * @author Gregor
-// */
 //public class ConnectionFactory {
 // private static interface Singleton {
 //  final ConnectionFactory INSTANCE = new ConnectionFactory();
