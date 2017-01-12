b5541ac20b93622241baaac6813ddaeff53f3443
Daniel <danielu96@hotmail.com>
2017-01-12 13:15:21 +0100

    Begin passwordreset

--- prototypefys/resetPassword.java
+++ prototypefys/resetPassword.java
@@ -1,4 +1,3 @@
-
 package prototypefys;
 
 import javafx.geometry.Pos;
@@ -11,22 +10,31 @@ import javafx.scene.layout.VBox;
  * @author Daniel
  */
 public class resetPassword {
-    
-    public resetPassword(){
-    
-}
-    
-    public StackPane maakPasswordReset(){
-        
+
+    public resetPassword() {
+
+    }
+
+    public StackPane maakPasswordReset() {
+
         StackPane basis = new StackPane();
-        
+
         HBox base = new HBox(200);
         base.setAlignment(Pos.CENTER);
 
         VBox vbox1 = new VBox(20);
         vbox1.setAlignment(Pos.CENTER);
-        
+
         return basis;
     }
-    
+
+    public static void main(String[] args) {
+        int generatedPass;
+
+        generatedPass = (int) (Math.random() * 999999 + 100000);
+
+        System.out.println(generatedPass);
+
+    }
+
 }
